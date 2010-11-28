9db78d1c3c348d4f92ae21adf1bbad5b6794bff4
Allen D. Ball <ball@hcf.dev>
2010-11-28 00:15:52 +0000

    Use CharSequenceOrder.CASE_INSENSITIVE.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.game@521 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/game/scrabble/OWL.java
+++ src/main/java/iprotium/game/scrabble/OWL.java
@@ -1,11 +1,11 @@
 /*
- * $Id: OWL.java,v 1.1 2010-11-24 05:17:26 ball Exp $
+ * $Id: OWL.java,v 1.2 2010-11-28 00:15:52 ball Exp $
  *
  * Copyright 2010 Allen D. Ball.  All rights reserved.
  */
 package iprotium.game.scrabble;
 
-import iprotium.util.Order;
+import iprotium.util.CharSequenceOrder;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.TreeSet;
@@ -14,12 +14,10 @@ import java.util.TreeSet;
  * Scrabble Official Word List ({@link OWL}).
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class OWL extends TreeSet<CharSequence> {
-    private static final long serialVersionUID = -3136908773362383664L;
-
-    private static final OrderImpl ORDER = new OrderImpl();
+    private static final long serialVersionUID = 8620121775973601775L;
 
     private final Locale locale;
 
@@ -34,7 +32,7 @@ public class OWL extends TreeSet<CharSequence> {
     public OWL(String language) { this(new Locale(language)); }
 
     private OWL(Locale locale) {
-        super(ORDER);
+        super(CharSequenceOrder.CASE_INSENSITIVE);
 
         if (locale != null) {
             this.locale = locale;
@@ -59,40 +57,7 @@ public class OWL extends TreeSet<CharSequence> {
     public CharSequence[] toArray() { return toArray(new CharSequence[] { }); }
 
     @Override
-    public OWL clone() /* throws CloneNotSupportedException */ {
-        return (OWL) super.clone();
-    }
-
-    private static class OrderImpl extends Order<CharSequence> {
-        private static final long serialVersionUID = -1592933537263187812L;
-
-        public OrderImpl() { super(); }
-
-        @Override
-        public int compare(CharSequence left, CharSequence right) {
-            int difference = 0;
-
-            if (difference == 0) {
-                int n = Math.min(left.length(), right.length());
-
-                for (int i = 0; i < n; i += 1) {
-                    difference =
-                        Character.toUpperCase(left.charAt(i))
-                        - Character.toUpperCase(right.charAt(i));
-
-                    if (difference != 0) {
-                        break;
-                    }
-                }
-            }
-
-            if (difference == 0) {
-                difference = left.length() - right.length();
-            }
-
-            return difference;
-        }
-    }
+    public OWL clone() { return (OWL) super.clone(); }
 }
 /*
  * $Log: not supported by cvs2svn $
