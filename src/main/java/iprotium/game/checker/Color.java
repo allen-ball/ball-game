71afa0b2942a74fa82965edc030718c5d869d7da
Allen D. Ball <ball@hcf.dev>
2013-07-27 16:03:01 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.game/trunk@1086 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/game/checker/Color.java
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.game.checker;
+
+/**
+ * Checker board and pieces' color {@link Enum} type.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision$
+ */
+public enum Color { BLACK, RED; }
---
+++ src/main/java/iprotium/game/chess/Color.java
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.game.chess;
+
+/**
+ * Chess board and pieces' color {@link Enum} type.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision$
+ */
+public enum Color { WHITE, BLACK; }
