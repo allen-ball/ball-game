181678baec8fc116544e9ba68caabcc8ba8a3d80
Allen D. Ball <ball@hcf.dev>
2014-07-10 14:20:46 +0000

    Made Grid base class abstract. Added constructor with Collection argument to specify Grid values. Added asList() method.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.game/trunk@1471 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/game/Grid.java
+++ src/main/java/ball/game/Grid.java
@@ -7,55 +7,58 @@ package ball.game;
 
 import ball.text.TableModel;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 import static java.util.Collections.unmodifiableList;
 
 /**
- * {@link Grid} base class.
+ * Abstract {@link Grid} base class.
  *
  * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
  * @version $Revision$
  */
-public class Grid<E> extends TableModel implements Iterable<E> {
-    private static final long serialVersionUID = -1232286055718384494L;
-
+public abstract class Grid<E> extends TableModel implements Iterable<E> {
     private final Class<E> type;
     private final List<List<E>> lists;
     private final List<Grid<E>> rows;
     private final List<Grid<E>> columns;
 
     /**
-     * Sole public constructor.
+     * Construct a {@link Grid} of the specified type.
      *
      * @param   height          The extent of the Y-axis.
      * @param   width           The extent of the X-axis.
      * @param   type            The {@link Class} of grid cell.
      */
-    public Grid(int height, int width, Class<E> type) {
-        this(height, width, type, null, -1, -1);
+    protected Grid(int height, int width, Class<E> type) {
+        this(height, width, type, null);
     }
 
     /**
-     * Protected constructor to {@link Grid} possibly backed by another
-     * {@link Grid}.
+     * Construct a {@link Grid} and populate with the specified values.
      *
      * @param   height          The extent of the Y-axis.
      * @param   width           The extent of the X-axis.
      * @param   type            The {@link Class} of grid cell.
-     * @param   grid            The backing {@link Grid} (may be
-     *                          {@code null}).
-     * @param   y0              The Y-coordinate of the upper lefthand
-     *                          corner of the sub-grid.
-     * @param   x0              The X-coordinate of the upper lefthand
-     *                          corner of the sub-grid.
+     * @param   collection      The {@link Collection} of grid cell values.
      */
-    protected Grid(int height, int width, Class<E> type,
-                   Grid<E> grid, int y0, int x0) {
+    protected Grid(int height, int width,
+                   Class<E> type, Collection<? extends E> collection) {
+        this(height, width, type, collection, null, -1, -1);
+    }
+
+    private Grid(int height, int width,
+                 Class<E> type, Collection<? extends E> collection,
+                 Grid<E> grid, int y0, int x0) {
         super(width);
 
-        this.type = (grid != null) ? grid.type : type;
+        if (type != null) {
+            this.type = type;
+        } else {
+            throw new NullPointerException("type");
+        }
 
         ArrayList<List<E>> lists = new ArrayList<List<E>>(height);
 
@@ -63,6 +66,12 @@ public class Grid<E> extends TableModel implements Iterable<E> {
             for (List<? extends E> list :
                      grid.lists.subList(y0, y0 + height)) {
                 lists.add(unmodifiableList(list.subList(x0, x0 + width)));
+            }
+        } else if (collection != null) {
+            Iterator<? extends E> iterator = collection.iterator();
+
+            for (int y = 0; y < height; y += 1) {
+                lists.add(unmodifiableList(new Row(width, iterator)));
             }
         } else {
             for (int y = 0; y < height; y += 1) {
@@ -88,6 +97,23 @@ public class Grid<E> extends TableModel implements Iterable<E> {
      * @return  The {@link List} of column {@link Grid}s.
      */
     public List<Grid<E>> columns() { return columns; }
+
+    /**
+     * Method to get the {@link List} of {@link Grid} values in row-column
+     * order.
+     *
+     * @return  The {@link List} of {@link Grid} values in row-column
+     *          order.
+     */
+    public List<E> asList() {
+        ArrayList<E> list = new ArrayList<E>(getRowCount() * getColumnCount());
+
+        for (List<? extends E> row : lists) {
+            list.addAll(row);
+        }
+
+        return list;
+    }
 
     /**
      * Method to get the {@link List} of sub-{@link Grid}s of with specific
@@ -152,18 +178,26 @@ public class Grid<E> extends TableModel implements Iterable<E> {
 
     @Override
     public Iterator<E> iterator() {
-        ArrayList<E> collection =
-            new ArrayList<E>(getRowCount() * getColumnCount());
-
-        for (List<? extends E> list : lists) {
-            collection.addAll(list);
-        }
-
-        return unmodifiableList(collection).iterator();
+        return unmodifiableList(asList()).iterator();
     }
 
+    @Override
+    public String toString() { return lists.toString(); }
+
     private class Row extends ArrayList<E> {
-        private static final long serialVersionUID = 6737750969514493847L;
+        private static final long serialVersionUID = -5170419862060802136L;
+
+        public Row(int width, Iterator<? extends E> iterator) {
+            super(width);
+
+            try {
+                for (int x = 0; x < width; x += 1) {
+                    add(iterator.next());
+                }
+            } catch (Exception exception) {
+                throw new ExceptionInInitializerError(exception);
+            }
+        }
 
         public Row(int width, Class<? extends E> type) {
             super(width);
@@ -179,10 +213,10 @@ public class Grid<E> extends TableModel implements Iterable<E> {
     }
 
     private class Sub extends Grid<E> {
-        private static final long serialVersionUID = -4134129332208780443L;
+        private static final long serialVersionUID = 2190407932893826313L;
 
         public Sub(int height, int width, int y0, int x0) {
-            super(height, width, null, Grid.this, y0, x0);
+            super(height, width, Grid.this.type, null, Grid.this, y0, x0);
         }
     }
 }
