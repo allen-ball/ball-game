eb6843db8163560c10835e76f9bc98caf7dfaaf3
Allen D. Ball <ball@hcf.dev>
2014-07-13 22:56:48 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.game/trunk@1484 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/game/scrabble/WordList.java
@@ -0,0 +1,47 @@
+/*
+ * $Id$
+ *
+ * Copyright 2014 Allen D. Ball.  All rights reserved.
+ */
+package ball.game.scrabble;
+
+import ball.util.CharSequenceOrder;
+import java.beans.ConstructorProperties;
+import java.util.Locale;
+import java.util.TreeSet;
+
+/**
+ * Scrabble Official Word List ({@link WordList}).
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class WordList extends TreeSet<CharSequence> {
+    private final Locale locale;
+
+    /**
+     * Sole constructor.
+     *
+     * @param   locale          The {@link Locale}.
+     */
+    @ConstructorProperties({ "locale" })
+    protected WordList(Locale locale) {
+        super(CharSequenceOrder.CASE_INSENSITIVE);
+
+        if (locale != null) {
+            this.locale = locale;
+        } else {
+            throw new NullPointerException("locale");
+        }
+    }
+
+    /**
+     * Method to get the {@link Locale} for this {@link WordList}.
+     *
+     * @return  The {@link Locale} for this {@link WordList}.
+     */
+    public Locale getLocale() { return locale; }
+
+    @Override
+    public CharSequence[] toArray() { return toArray(new CharSequence[] { }); }
+}
