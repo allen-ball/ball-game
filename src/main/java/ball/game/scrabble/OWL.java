d0eebac78a410b024187a0abd5a87b9b943b6f97
Allen D. Ball <ball@hcf.dev>
2014-07-13 22:59:27 +0000

    Re-factored as a subclass of WordList.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.game/trunk@1485 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/game/scrabble/OWL.java
+++ src/main/java/ball/game/scrabble/OWL.java
@@ -5,57 +5,27 @@
  */
 package ball.game.scrabble;
 
-import ball.util.CharSequenceOrder;
 import java.util.Locale;
 import java.util.ResourceBundle;
-import java.util.TreeSet;
 
 /**
- * Scrabble Official Word List ({@link OWL}).
+ * Scrabble Official {@link WordList} ({@link OWL}).
  *
  * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
  * @version $Revision$
  */
-public class OWL extends TreeSet<CharSequence> implements Cloneable {
-    private static final long serialVersionUID = 2941475666048685465L;
-
-    private final Locale locale;
+public class OWL extends WordList {
+    private static final long serialVersionUID = -6945700016060061297L;
 
     /**
-     * No-argument constructor.
+     * Sole constructor.
      */
-    public OWL() { this(Locale.ENGLISH); }
-
-    /**
-     * @param   language        The {@link Locale} language.
-     */
-    public OWL(String language) { this(new Locale(language)); }
-
-    private OWL(Locale locale) {
-        super(CharSequenceOrder.CASE_INSENSITIVE);
-
-        if (locale != null) {
-            this.locale = locale;
-        } else {
-            throw new NullPointerException("locale");
-        }
+    public OWL() {
+        super(Locale.ENGLISH);
 
         ResourceBundle bundle =
             ResourceBundle.getBundle(getClass().getName(), getLocale());
 
         addAll(bundle.keySet());
     }
-
-    /**
-     * Method to get the {@link Locale} for this {@link OWL}.
-     *
-     * @return  The {@link Locale} for this {@link OWL}.
-     */
-    public Locale getLocale() { return locale; }
-
-    @Override
-    public CharSequence[] toArray() { return toArray(new CharSequence[] { }); }
-
-    @Override
-    public OWL clone() { return (OWL) super.clone(); }
 }
