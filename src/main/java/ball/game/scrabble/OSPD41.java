ea5d0db1f02a6653743374863d9aa57107701805
Allen D. Ball <ball@hcf.dev>
2016-10-22 14:05:28 +0000

    Added word lists.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.game/trunk@2006 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/game/scrabble/OSPD41.java
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ *
+ * Copyright 2016 Allen D. Ball.  All rights reserved.
+ */
+package ball.game.scrabble;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Scrabble {@link OSPD41} {@link WordList}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public class OSPD41 extends WordList {
+    private static final long serialVersionUID = -6267940304364188970L;
+
+    /**
+     * Sole constructor.
+     */
+    public OSPD41() {
+        super(Locale.ENGLISH);
+
+        ResourceBundle bundle =
+            ResourceBundle.getBundle(getClass().getName(), getLocale());
+
+        for (String key : bundle.keySet()) {
+            add(key.split("[\\p{Space}]+", 2)[0]);
+        }
+    }
+}
---
+++ src/main/java/ball/game/scrabble/OWL21.java
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ *
+ * Copyright 2016 Allen D. Ball.  All rights reserved.
+ */
+package ball.game.scrabble;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Scrabble Official {@link WordList} ({@link OWL21}).
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public class OWL21 extends WordList {
+    private static final long serialVersionUID = 1381786432796311780L;
+
+    /**
+     * Sole constructor.
+     */
+    public OWL21() {
+        super(Locale.ENGLISH);
+
+        ResourceBundle bundle =
+            ResourceBundle.getBundle(getClass().getName(), getLocale());
+
+        for (String key : bundle.keySet()) {
+            add(key.split("[\\p{Space}]+", 2)[0]);
+        }
+    }
+}
