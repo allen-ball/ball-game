e3c862622fa9b400d190f3979088de14e9d7bf6e
Allen D. Ball <ball@hcf.dev>
2019-03-12 04:02:12 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.game/trunk@3852 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/game/card/Cards.java
@@ -0,0 +1,141 @@
+/*
+ * $Id$
+ *
+ * Copyright 2019 Allen D. Ball.  All rights reserved.
+ */
+package ball.game.card;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * {@link Card} static methods and fields.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class Cards {
+    private Cards() { }
+
+    /**
+     * {@link Predicate} to test all {@link Card}s are the same
+     * {@link Color}.
+     */
+    public static final Predicate<List<Card>> SAME_COLOR =
+        t -> ((! t.isEmpty())
+              && t.stream().allMatch(Card.isSameColorAs(t.get(0))));
+
+    /**
+     * {@link Predicate} to test all {@link Card}s are the same
+     * {@link Rank}.
+     */
+    public static final Predicate<List<Card>> SAME_RANK =
+        t -> ((! t.isEmpty())
+              && t.stream().allMatch(Card.isSameRankAs(t.get(0))));
+
+    /**
+     * {@link Predicate} to test all {@link Card}s are the same
+     * {@link Suit}.
+     */
+    public static final Predicate<List<Card>> SAME_SUIT =
+        t -> ((! t.isEmpty())
+              && t.stream().allMatch(Card.isSameSuitAs(t.get(0))));
+
+    /**
+     * {@link Predicate} to test the {@link Card}s make up a sequence.
+     */
+    public static final Predicate<List<Card>> SEQUENCE =
+        t -> ((! t.isEmpty())
+              && (isSequenced(t, c -> c.getRank().rank())
+                  || isSequenced(t, c -> c.getRank().ordinal())));
+
+    private static boolean isSequenced(List<Card> list,
+                                       Function<Card,Integer> mapper) {
+        return (list.stream()
+                .map(mapper)
+                .collect(Collectors.toList())
+                .equals(IntStream.iterate(list.stream()
+                                          .map(mapper)
+                                          .findFirst().orElse(0),
+                                          t -> t - 1)
+                        .boxed()
+                        .limit(list.size())
+                        .collect(Collectors.toList())));
+    }
+
+    /**
+     * Method to return a {@link Predicate} to test if all {@link Card}s are
+     * the specified {@link Color}.
+     *
+     * @param   color           The {@link Color}.
+     *
+     * @return  {@link Predicate}
+     */
+    public static Predicate<List<Card>> allMatch(Color color) {
+        return t -> t.stream().allMatch(Card.is(color));
+    }
+
+    /**
+     * Method to return a {@link Predicate} to test if any {@link Card}s are
+     * the specified {@link Color}.
+     *
+     * @param   color           The {@link Color}.
+     *
+     * @return  {@link Predicate}
+     */
+    public static Predicate<List<Card>> anyMatch(Color color) {
+        return t -> t.stream().anyMatch(Card.is(color));
+    }
+
+    /**
+     * Method to return a {@link Predicate} to test if all {@link Card}s are
+     * the specified {@link Rank}.
+     *
+     * @param   rank            The {@link Rank}.
+     *
+     * @return  {@link Predicate}
+     */
+    public static Predicate<List<Card>> allMatch(Rank rank) {
+        return t -> t.stream().allMatch(Card.is(rank));
+    }
+
+    /**
+     * Method to return a {@link Predicate} to test if any {@link Card}s are
+     * the specified {@link Rank}.
+     *
+     * @param   rank            The {@link Rank}.
+     *
+     * @return  {@link Predicate}
+     */
+    public static Predicate<List<Card>> anyMatch(Rank rank) {
+        return t -> t.stream().anyMatch(Card.is(rank));
+    }
+
+    /**
+     * Method to return a {@link Predicate} to test if all {@link Card}s are
+     * the specified {@link Suit}.
+     *
+     * @param   suit            The {@link Suit}.
+     *
+     * @return  {@link Predicate}
+     */
+    public static Predicate<List<Card>> allMatch(Suit suit) {
+        return t -> t.stream().allMatch(Card.is(suit));
+    }
+
+    /**
+     * Method to return a {@link Predicate} to test if any {@link Card}s are
+     * the specified {@link Suit}.
+     *
+     * @param   suit            The {@link Suit}.
+     *
+     * @return  {@link Predicate}
+     */
+    public static Predicate<List<Card>> anyMatch(Suit suit) {
+        return t -> t.stream().anyMatch(Card.is(suit));
+    }
+}
