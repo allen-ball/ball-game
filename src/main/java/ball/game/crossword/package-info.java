7469ced71145ed47e4ecaca544d0dc776c9e4564
Allen D. Ball <ball@hcf.dev>
2019-06-20 05:23:01 +0000

    Updated external links.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.game/trunk@4212 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/game/crossword/package-info.java
+++ src/main/java/ball/game/crossword/package-info.java
@@ -5,6 +5,12 @@
  */
 /**
  * Classes for creating, analyzing, and solving crossword puzzles.
+ * The file format is adopted from
+ * {@link.uri https://github.com/century-arcade/xd target=newtab https://github.com/century-arcade/xd}.
+ * The {@link.uri http://www.preshortzianpuzzleproject.com/ target=newtab The Pre-Shortzian Puzzle Project}
+ * also provides inspiration for this package.  Finally, this
+ * {@link.uri https://www.nytimes.com/2018/12/18/crosswords/new-york-times-crossword-contributors-to-see-pay-raise-after-jan-1.html target=newtab article}
+ * provided this author's inspiration.
  *
  * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
  */
