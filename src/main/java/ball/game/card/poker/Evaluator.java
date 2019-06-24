15a114a2287fcc3f9bb1ca8360cb73554374f54e
Allen D. Ball <ball@hcf.dev>
2019-06-24 14:10:59 +0000

    Removed superfluous import.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.game/trunk@4237 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/game/card/poker/Evaluator.java
+++ src/main/java/ball/game/card/poker/Evaluator.java
@@ -17,7 +17,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
