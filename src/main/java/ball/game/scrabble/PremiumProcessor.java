f19bf630918821c948dc08f72d118caca81d9443
Allen D. Ball <ball@hcf.dev>
2020-05-03 16:25:31 +0000

    AbstractAnnotationProcessor -> AnnotatedProcessor, AbstractNoAnnotationProcessor -> AnnotatedNoAnnotationProcessor; Re-factored class hierarchy accordingly.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-game/trunk@5873 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/game/scrabble/PremiumProcessor.java
+++ src/main/java/ball/game/scrabble/PremiumProcessor.java
@@ -21,7 +21,7 @@ package ball.game.scrabble;
  * ##########################################################################
  */
 import ball.annotation.ServiceProviderFor;
-import ball.annotation.processing.AbstractAnnotationProcessor;
+import ball.annotation.processing.AnnotatedProcessor;
 import ball.annotation.processing.For;
 import java.util.Set;
 import javax.annotation.processing.Processor;
@@ -53,7 +53,7 @@ import static javax.tools.Diagnostic.Kind.ERROR;
 @ServiceProviderFor({ Processor.class })
 @For({ LetterPremium.class, WordPremium.class })
 @NoArgsConstructor @ToString
-public class PremiumProcessor extends AbstractAnnotationProcessor {
+public class PremiumProcessor extends AnnotatedProcessor {
     @Override
     public void process(RoundEnvironment roundEnv,
                         TypeElement annotation, Element element) {
